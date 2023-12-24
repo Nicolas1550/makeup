8bc1e32cad15ff764f092262ef2f44265df5cca4
sofia1550 <luccciuksofia@gmail.com>
2023-12-24 02:30:31 -0300

    asd

--- next.config.js
+++ next.config.js
@@ -5,15 +5,17 @@ const nextConfig = {
   },
   images: {
     domains: [
-      'localhost:3002', 
+      'localhost:3002',
       'localhost:3005',
-      'localhost', 
+      'localhost',
       'tse1.mm.bing.net',
       'tse4.mm.bing.net',
       'tse2.mm.bing.net',
       'example.com',
+      'https://makeup-auh2.vercel.app',
+
       '65180f852cde840008661f9c--curious-buttercream-52ba76.netlify.app',
-      'https://maps.googleapis.com/maps/api/js/QuotaService.RecordEvent?1shttps%3A%2F%2Fwww.google.com%2Fmaps%2Fembed&2sgoogle-maps-embed&7serfz08&10e1&11b1&callback=_xdc_._a82259&client=google-maps-embed&token=122749', 
+      'https://maps.googleapis.com/maps/api/js/QuotaService.RecordEvent?1shttps%3A%2F%2Fwww.google.com%2Fmaps%2Fembed&2sgoogle-maps-embed&7serfz08&10e1&11b1&callback=_xdc_._a82259&client=google-maps-embed&token=122749',
     ],
   },
   webpack: (config, options) => {
