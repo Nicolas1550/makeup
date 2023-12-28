23ece0e7fac2a01309da9166493eb8caf8c8c56b
sofia1550 <luccciuksofia@gmail.com>
2023-12-28 03:05:21 -0300

    f

--- src/app/components/contactPage/contactPage.tsx
+++ src/app/components/contactPage/contactPage.tsx
@@ -112,8 +112,7 @@ const ContactPage: React.FC = () => {
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
-          <SubmitButton type="submit">Enviar Mensajeeee</SubmitButton>
-          <SubmitButton type="submit">Enviar Mensajeeee</SubmitButton>
+          <SubmitButton type="submit">Enviar Mensaje</SubmitButton>
 
         </Form>
       </ContactContainer>
